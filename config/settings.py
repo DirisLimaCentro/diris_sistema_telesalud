9edc182f31b45f1b509f9988cec4722d5ce51c87
JONATHAN RICARDO RUELAS RAMIREZ <jonathan.ruelas@DIRISLC.GOB.PE>
2026-03-06 12:17:35 -0500

    .

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b!v9kq8di6qxz4@yo2j$j#syonw)mal++gn(9sqm%+7!h!&vpk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
